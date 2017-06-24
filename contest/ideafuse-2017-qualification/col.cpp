91acd00bdce71e8dd03d2294fc24356395aaee8f
Kinto D <kiswanto.d21@gmail.com>
2017-06-24 22:52:42 +0700

    add some contest history

---
+++ contest/ideafuse-2017-qualification/col.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// game implementation about resource
+
+int main() {
+    int cases;
+    cin >> cases;
+    for (int i = 0; i < cases; i++) {
+        long need = 0;
+        long current_resources = 0;
+        int tiers;
+        cin >> tiers;
+        for (int j = 0; j < tiers; j++) {
+            long next, bonus;
+            cin >> next >> bonus;
+            if (current_resources < next) {
+                need += next - current_resources;
+                current_resources += next - current_resources + bonus;
+            } else {
+                current_resources += bonus;
+            }
+        }
+        cout << "Case #" << i + 1 << ": " << need << endl;
+    }
+    return 0;
+}
